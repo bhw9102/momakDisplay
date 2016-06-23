7c1b10789fd44943ce1cd7ef534baba64d1ee9d9
Baehyunwoo <bhw9102@gmail.com>
2016-06-24 08:46:54 +0900

    웹소켓 통신 모듈은 넣다.

---
+++ Assets/Scripts/Market.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using WebSocketSharp;
+using LitJson;
+
+public class Market : MonoBehaviour {
+    public string ip = "192.168.0.101";
+    public string port = "8000";
+    public string target = "market-display";
+    public int marketIndex = 1;
+
+    public Renderer display;
+
+    private WebSocket ws;
+    private JsonData jockeyState;
+
+    // Use this for initialization
+    IEnumerator Start() {
+        ws = new WebSocket("ws://192.168.0.103:8000/marketws/1");
+
+        yield return ws;
+
+        ws.OnMessage += (sender, e) => {
+            Debug.Log(e.Data);
+        };
+        ws.Connect();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
